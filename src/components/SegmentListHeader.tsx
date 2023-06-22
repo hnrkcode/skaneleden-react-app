01b390fb0a93a791f2fe0b04b76afbe4e994a261
Henrik Petersson <44243358+hnrkcode@users.noreply.github.com>
2023-06-22 19:31:01 +0200

    Fix so the active ordering arrow is highlighted by making it darker

--- src/components/SegmentListHeader.tsx
+++ src/components/SegmentListHeader.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import SegmentListHeaderTitle from "./SegmentListHeaderTitle";
 
 interface SegmentListHeaderProps {
@@ -5,8 +6,23 @@ interface SegmentListHeaderProps {
 }
 
 export default function SegmentListHeader({ onSort }: SegmentListHeaderProps) {
+  const [activeArrow, setActiveArrow] = useState<string | null>(null);
+  const [activeColumn, setActiveColumn] = useState<string | null>(null);
+
+  function handleClick(column: string, order: string) {
+    setActiveColumn(column);
+    setActiveArrow(order);
+    onSort(column, order);
+  }
+
   const titles = ["Segment", "Track", "Distance", "Difficulty"].map((title) => (
-    <SegmentListHeaderTitle key={title} title={title} onSort={onSort} />
+    <SegmentListHeaderTitle
+      key={title}
+      title={title}
+      onSort={handleClick}
+      activeArrow={activeArrow}
+      activeColumn={activeColumn}
+    />
   ));
 
   return <div className="flex flex-row px-4">{titles}</div>;
--- src/components/SegmentListHeaderTitle.tsx
+++ src/components/SegmentListHeaderTitle.tsx
@@ -1,18 +1,40 @@
 interface SegmentListHeaderTitleProps {
   title: string;
   onSort: (column: string, order: string) => void;
+  activeArrow: string | null;
+  activeColumn: string | null;
 }
 
 export default function SegmentListHeaderTitle({
   title,
   onSort,
+  activeArrow,
+  activeColumn,
 }: SegmentListHeaderTitleProps) {
   return (
     <div className="flex flex-row basis-1/4 items-center gap-2">
       <h2 className="font-bold mb-2">{title}</h2>
       <div className="text-xs leading-3 cursor-pointer select-none">
-        <div onClick={() => onSort(title, "asc")}>&#9650;</div>
-        <div onClick={() => onSort(title, "desc")}>&#9660;</div>
+        <div
+          className={
+            activeArrow === "asc" && activeColumn === title
+              ? "text-gray-800"
+              : "text-gray-400"
+          }
+          onClick={() => onSort(title, "asc")}
+        >
+          &#9650;
+        </div>
+        <div
+          className={
+            activeArrow === "desc" && activeColumn === title
+              ? "text-gray-800"
+              : "text-gray-400"
+          }
+          onClick={() => onSort(title, "desc")}
+        >
+          &#9660;
+        </div>
       </div>
     </div>
   );
